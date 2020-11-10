10def74e4709f49a127462449ba9543a25b367dc
DanielGutierrez <daniel7dng@hotmail.com>
2020-11-09 23:20:57 -0300

    inicializado

---
+++ server/server.js
@@ -0,0 +1,43 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.json('Hello World');
+});
+
+app.get('/usuarios', function (req, res) {
+    res.json('get usuarios');
+  });
+
+app.post('/usuarios', function (req, res) {
+    let body = req.body; 
+    if(body.nombre === undefined){
+        res.status(400).json({
+            ok: false,
+            mensaje: 'el nombre es necesario'
+        });
+    }else{
+        res.json({
+            persona: body
+        });
+    }
+});
+
+app.put('/usuarios/:id', function (req, res) {
+    let id = req.params.id;  
+    res.json({
+        id
+    });
+  });  
+ 
+app.listen(process.env.PORT,()=>{
+    console.log('escuchando puerto: ',process.env.PORT);
+});
